1511285a6aec15f91e8dfc9855d3819cfecfceb4
OnCallMarketing.biz <vbeckett@oncallmarketing.biz>
2018-06-30 18:27:13 +1200

    Generate 5 components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ListAllComponent } from './list-all/list-all.component';
+import { ListByUserComponent } from './list-by-user/list-by-user.component';
+import { ViewAppointmentComponent } from './view-appointment/view-appointment.component';
+import { EditAppointmentComponent } from './edit-appointment/edit-appointment.component';
+import { AddNoteComponent } from './add-note/add-note.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListAllComponent,
+    ListByUserComponent,
+    ViewAppointmentComponent,
+    EditAppointmentComponent,
+    AddNoteComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/list-by-user/list-by-user.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-by-user',
+  templateUrl: './list-by-user.component.html',
+  styleUrls: ['./list-by-user.component.css']
+})
+export class ListByUserComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
