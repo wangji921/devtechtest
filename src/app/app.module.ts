c30e70beb62459134cd8e4703fbc34d04348a52b
wangji921 <vbeckett@oncallmarketing.biz>
2018-07-01 17:11:19 +1200

    remove unused components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,6 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ListAllComponent } from './list-all/list-all.component';
 import { ListByUserComponent } from './list-by-user/list-by-user.component';
-import { ViewAppointmentComponent } from './view-appointment/view-appointment.component';
-import { EditAppointmentComponent } from './edit-appointment/edit-appointment.component';
 import { AddNoteComponent } from './add-note/add-note.component';
 import { AppointmentListComponent } from './appointment-list/appointment-list.component';
 import { FilterPipe } from './filter.pipe';
@@ -17,8 +15,6 @@ import { FilterPipe } from './filter.pipe';
     AppComponent,
     ListAllComponent,
     ListByUserComponent,
-    ViewAppointmentComponent,
-    EditAppointmentComponent,
     AddNoteComponent,
     AppointmentListComponent,
     FilterPipe
